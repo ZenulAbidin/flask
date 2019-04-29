e25c46aa632641b27130311db848c8cbde90df61
ZenulAbidin <42585895+ZenulAbidin@users.noreply.github.com>
2019-04-29 20:23:50 +0200

    Version 2c
    Signed-off-by: ZenulAbidin <42585895+ZenulAbidin@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {"username": "Miguel"}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user)
